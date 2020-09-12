d1957fde7ce3bea303497e09e4fcf42af3946ec7
徐瑞超 <xrcdev@163.com>
2020-09-12 21:47:53 +0800

    泛型的协变

--- GenenicVarianceTest/Program.cs
+++ GenenicVarianceTest/Program.cs
@@ -12,24 +12,25 @@ namespace GenenicVarianceTest
         {
             //Cat<int> cat = new Animal<int>();
 
-            Animal<int, DateTime> animal = new Animal<int, DateTime>();
+            Animal<int, string> animal = new Cat<int, string>();
             animal.Walk(5);
-            animal.Drive<string>(20, "bike");
+            animal.Drive(20, "bike");
             Console.ReadLine();
         }
     }
 
-    interface IWalk<in SpeedT, out vehicleT> where vehicleT : new()
+    interface IMove<in SpeedT, in vehicleT>
     {
         void Walk(SpeedT speed);
-        vehicleT Drive(SpeedT t);
+        string Drive(SpeedT speed, vehicleT vType);
     }
-
-    public class Animal<SpeedT, vehicleT> : IWalk<SpeedT, vehicleT> where vehicleT : new()
+    // new() 可以作用到 datatime ,是引用类型?
+    //public class Animal<SpeedT, vehicleT> : IWalk<SpeedT, vehicleT> where vehicleT : new()
+    public class Animal<SpeedT, vehicleT> : IMove<SpeedT, vehicleT>
     {
-        public Animal()
-        {
-        }
+        //public Animal()
+        //{
+        //}
 
         public string Name { get; set; }
 
@@ -54,31 +55,51 @@ namespace GenenicVarianceTest
         //    return vehicleType;
         //}
 
-        void IWalk<SpeedT, vehicleT>.Walk(SpeedT speed)
+
+
+        //public void Drive(SpeedT speed)
+        //{
+        //    System.Diagnostics.Debug.WriteLine($"Animal:{Name} walk speed: {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
+        //}
+
+        //public void IMove<SpeedT, vehicleT>.Walk(SpeedT speed)
+        public void Walk(SpeedT speed)
         {
             System.Diagnostics.Debug.WriteLine($"Animal:{Name} walk speed: {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
         }
 
-        public vehicleT Drive(SpeedT t)
+        public string Drive(SpeedT speed, vehicleT vType)
         {
-            System.Diagnostics.Debug.WriteLine($"Animal:{Name} walk speed: {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
+            System.Diagnostics.Debug.WriteLine($"Animal:{Name} rides a {vType} on the road at a speed of {speed}km/h! This Type:{this.GetType()} , speed Type {speed.GetType()}, vType Type {vType.GetType()}");
+            return vType.ToString() + speed.ToString();
         }
     }
 
-    //public class Cat<T> : Animal<T>
-    //{
-    //    public string Name { get; set; }
+    public class Cat<SpeedT, vehicleT> : Animal<SpeedT, vehicleT>
+    {
+        public string Name { get; set; }
 
-    //    public new void Walk(T speed)
-    //    {
-    //        System.Diagnostics.Debug.WriteLine($"Cat :{Name} walk spend : {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
-    //    }
+        //public new void Walk(T speed)
+        //{
+        //    System.Diagnostics.Debug.WriteLine($"Cat :{Name} walk spend : {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
+        //}
 
-    //    public new void Drive<S>(T speed, S s)
-    //    {
-    //        System.Diagnostics.Debug.WriteLine($"Cat:{Name} rides a {s} on the road at a speed of {speed}km/h! This Type:{this.GetType()} , obj Type {speed.GetType()}, S Type {s.GetType()}");
-    //    }
-    //}
+        //public new void Drive<S>(T speed, S s)
+        //{
+        //    System.Diagnostics.Debug.WriteLine($"Cat:{Name} rides a {s} on the road at a speed of {speed}km/h! This Type:{this.GetType()} , obj Type {speed.GetType()}, S Type {s.GetType()}");
+        //}
+
+        public void Walk(SpeedT speed)
+        {
+            System.Diagnostics.Debug.WriteLine($"Cat:{Name} walk speed: {speed}km/h ! This Type:{this.GetType()} , obj Type {speed.GetType()}");
+        }
+
+        public string Drive(SpeedT speed, vehicleT vType)
+        {
+            System.Diagnostics.Debug.WriteLine($"Cat:{Name} rides a {vType} on the road at a speed of {speed}km/h! This Type:{this.GetType()} , speed Type {speed.GetType()}, vType Type {vType.GetType()}");
+            return vType.ToString() + speed.ToString();
+        }
+    }
 
 
 }
